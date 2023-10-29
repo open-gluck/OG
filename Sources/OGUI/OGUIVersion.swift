af1e3221dbeb2e975aa6c34b07e68c4318e2c574
Christopher Allène <christopher.allene@mobsuccess.com>
2023-10-29 21:22:38 +0000

    Bump versions in Swift files

--- Sources/OGUI/OGUIVersion.swift
+++ Sources/OGUI/OGUIVersion.swift
@@ -1,5 +1,5 @@
 // this file is generated automatically by the build system
 // do not modify this file manually
 public extension OGUI {
-    static let VERSION = "v1.0.79"
+    static let VERSION = "v1.0.80"
 }
