d4b4956f82e92f656ad2978ab21aca798cb04251
Christopher Allène <christopher.allene@mobsuccess.com>
2023-10-29 21:15:58 +0000

    Bump versions in Swift files

--- Sources/OG/OGVersion.swift
+++ Sources/OG/OGVersion.swift
@@ -1,5 +1,5 @@
 // this file is generated automatically by the build system
 // do not modify this file manually
 public extension OG {
-    static let VERSION = "v1.0.78"
+    static let VERSION = "v1.0.77"
 }
