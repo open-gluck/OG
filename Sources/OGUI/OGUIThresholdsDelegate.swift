7d9f0bb9cfd3386c3361e0f0aad6db94faf985f6
Christopher Allène <christopher.allene@mobsuccess.com>
2023-10-30 08:00:21 +0000

    Swift OGUIThresholdsDelegate to a protocol

--- Sources/OGUI/OGUIThresholdsDelegate.swift
+++ Sources/OGUI/OGUIThresholdsDelegate.swift
@@ -1,21 +1,21 @@
 import Foundation
 
-protocol OGUIThresholdsDelegate {
+public protocol OGUIThresholdsDelegate {
     // when the blood glucose is below normalHigh, it is still considered
     // in the normal range until it reaches low, but in the low range
-    public var normalLow: Double
+    var normalLow: Double { get }
 
     // when the blood glucose is above normalHigh, it is still considered
     // in the normal range until it reaches high, but in the high range
-    public var normalHigh: Double
+    var normalHigh: Double { get }
 
     // when the blood glucose is below low, it is considered low
-    public var low: Double
+    var low: Double { get }
 
     // when the blood glucose is at or above high, it is considered high
-    public var high: Double
+    var high: Double { get }
 
     // when the blood glucose is at or above highVeryHigh, it is considered
     // very high
-    public var highVeryHigh: Double
+    var highVeryHigh: Double { get }
 }
