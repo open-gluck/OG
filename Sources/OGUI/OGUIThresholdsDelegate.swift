9aef4db75b4b1ed069e62e122f95daae58d434ab
Christopher Allène <christopher.allene@mobsuccess.com>
2023-10-30 07:48:13 +0000

    Add missing file

---
+++ Sources/OGUI/OGUIThresholdsDelegate.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+open class OGUIThresholdsDelegate {
+    public init() {}
+
+    // when the blood glucose is below normalHigh, it is still considered
+    // in the normal range until it reaches low, but in the low range
+    public var normalLow: Double {
+        70.0
+    }
+
+    // when the blood glucose is above normalHigh, it is still considered
+    // in the normal range until it reaches high, but in the high range
+    public var normalHigh: Double {
+        150.0
+    }
+
+    // when the blood glucose is below low, it is considered low
+    public var low: Double {
+        70.0
+    }
+
+    // when the blood glucose is at or above high, it is considered high
+    public var high: Double {
+        170.0
+    }
+
+    // when the blood glucose is at or above highVeryHigh, it is considered
+    // very high
+    public var highVeryHigh: Double {
+        240.0
+    }
+}
