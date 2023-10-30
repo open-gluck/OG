07e72eb0cb03a44b41feaaa098aa4a7fbc5076e6
Christopher Allène <christopher.allene@mobsuccess.com>
2023-10-30 08:09:58 +0000

    Add better defaults for thresholds

--- Sources/OGUI/DefaultOGUIThresholdsDelegate.swift
+++ Sources/OGUI/DefaultOGUIThresholdsDelegate.swift
@@ -1,32 +1,38 @@
 
-class DefaultOGUIThresholdsDelegate: OGUIThresholdsDelegate {
+public class DefaultOGUIThresholdsDelegate: OGUIThresholdsDelegate {
     public init() {}
+
+    public static let defaultNormalLow: Double = 70.0
+    public static let defaultNormalHigh: Double = 150.0
+    public static let defaultLow: Double = 70.0
+    public static let defaultHigh: Double = 170.0
+    public static let defaultHighVeryHigh: Double = 240.0
 
     // when the blood glucose is below normalHigh, it is still considered
     // in the normal range until it reaches low, but in the low range
     public var normalLow: Double {
-        70.0
+        Self.defaultNormalLow
     }
 
     // when the blood glucose is above normalHigh, it is still considered
     // in the normal range until it reaches high, but in the high range
     public var normalHigh: Double {
-        150.0
+        Self.defaultNormalHigh
     }
 
     // when the blood glucose is below low, it is considered low
     public var low: Double {
-        70.0
+        Self.defaultLow
     }
 
     // when the blood glucose is at or above high, it is considered high
     public var high: Double {
-        170.0
+        Self.defaultHigh
     }
 
     // when the blood glucose is at or above highVeryHigh, it is considered
     // very high
     public var highVeryHigh: Double {
-        240.0
+        Self.defaultHighVeryHigh
     }
 }
