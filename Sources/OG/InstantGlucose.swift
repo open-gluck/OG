4164e23e1485d2c35b11c3042d8e9b8f36ca10f7
Christopher Allène <christopher.allene@mobsuccess.com>
2024-05-26 07:36:49 +0000

    feat: add instant glucose records to last

--- Sources/OG/InstantGlucose.swift
+++ Sources/OG/InstantGlucose.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public struct OpenGluckInstantGlucoseRecord: Hashable, Codable {
+public struct OpenGluckInstantGlucoseRecord: Hashable, Codable, Sendable {
     public let timestamp: Date
     public let mgDl: Int
     public let modelName: String
