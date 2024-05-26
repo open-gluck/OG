c19e7b5a42297125eb898a7f51e943b486b33970
Christopher Allène <christopher.allene@mobsuccess.com>
2024-05-26 08:51:00 +0000

    feat: add current instant glucose record in current data

--- Sources/OG/Current.swift
+++ Sources/OG/Current.swift
@@ -2,6 +2,7 @@ import Foundation
 
 public struct CurrentData: Codable {
     public let currentGlucoseRecord: OpenGluckGlucoseRecord?
+    public let currentInstantGlucoseRecord: OpenGluckInstantGlucoseRecord?
     public let lastHistoricGlucoseRecord: OpenGluckGlucoseRecord?
     public let currentEpisode: Episode
     public let currentEpisodeTimestamp: Date
@@ -10,6 +11,7 @@ public struct CurrentData: Codable {
 
     enum CodingKeys: String, CodingKey {
         case currentGlucoseRecord = "current_glucose_record"
+        case currentInstantGlucoseRecord = "current_instant_glucose_record"
         case lastHistoricGlucoseRecord = "last_historic_glucose_record"
         case revision
         case currentEpisode = "current_episode"
@@ -20,6 +22,7 @@ public struct CurrentData: Codable {
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         currentGlucoseRecord = try container.decodeIfPresent(OpenGluckGlucoseRecord.self, forKey: .currentGlucoseRecord)
+        currentInstantGlucoseRecord = try container.decodeIfPresent(OpenGluckInstantGlucoseRecord.self, forKey: .currentInstantGlucoseRecord)
         currentEpisode = try container.decodeIfPresent(Episode.self, forKey: .currentEpisode) ?? .unknown
         currentEpisodeTimestamp = try container.decodeIfPresent(Date.self, forKey: .currentEpisodeTimestamp) ?? Date(timeIntervalSince1970: 0)
         lastHistoricGlucoseRecord = try container.decodeIfPresent(OpenGluckGlucoseRecord.self, forKey: .lastHistoricGlucoseRecord)
@@ -31,6 +34,9 @@ public struct CurrentData: Codable {
         var container = encoder.container(keyedBy: CodingKeys.self)
         if let currentGlucoseRecord {
             try container.encode(currentGlucoseRecord, forKey: .currentGlucoseRecord)
+        }
+        if let currentInstantGlucoseRecord {
+            try container.encode(currentInstantGlucoseRecord, forKey: .currentInstantGlucoseRecord)
         }
         try container.encode(currentEpisode, forKey: .currentEpisode)
         try container.encode(currentEpisodeTimestamp, forKey: .currentEpisodeTimestamp)
